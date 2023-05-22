6e6cb58e13096f4ceffd940f2bcbfbd5b77e3a55
Pedro Minicz <pedrominicz@gmail.com>
2023-05-22 11:57:49 -0300

    Project Euler problems 1 and 2

--- euler/01.c
+++ euler/01.c
@@ -9,9 +9,7 @@ Find the sum of all the multiples of 3 or 5 below 1000.
 
 */
 
-int entrada = 1000;
-
-int main(void) {
+int f0(int entrada) {
   int x = entrada - 1;
 
   int soma_total = 0;
@@ -23,7 +21,50 @@ int main(void) {
     x = x - 1;
   }
 
-  printf("%d\n", soma_total);
+  return soma_total;
+}
+
+int f1(int entrada) {
+  int x = entrada - 1;
+  int y = 0;
+
+  do {
+    if ((x % 3 == 0) || (x % 5 == 0)) {
+      y = y + x;
+    }
+    x = x - 1;
+  } while (x > 0);
+
+  return y;
+}
+
+int f2(int entrada) {
+  int x;
+  int y = 0;
+  int z;
+
+  x = entrada;
+  z = x - 1;
+
+  for (x = z; x > 0; x = x - 1) {
+    if ((x % 3 == 0) || (x % 5 == 0)) {
+      y = y + x;
+    }
+  }
+
+  return y;
+}
+
+int main(void) {
+  for (int i = 0; i <= 10000; ++i) {
+    int x0 = f0(i);
+    int x1 = f1(i);
+    int x2 = f2(i);
+    if (x0 != x1) printf("f1(%d) = %d\n", i, x1);
+    if (x0 != x2) printf("f2(%d) = %d\n", i, x2);
+  }
+
+  printf("%d\n", f0(1000));
 
   return 0;
 }
---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// gcc main.c
+// ./a.out
+
+int main(void) {
+  int x = 5;
+
+  printf("x = %d\n", x);
+
+  return 0;
+}
